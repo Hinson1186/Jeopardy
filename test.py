d56877d4da620fe8a6b9e1a347b00421156540ac
finencoarse <ivanwong714@gmail.com>
2026-04-14 23:30:16 +0800

    Refactor test.py by adding Actor and Player classes, and fix print statement

--- test.py
+++ test.py
@@ -2,3 +2,20 @@
 name = ["a", "b", "c"]
 print(f"{name} is here")
 print(Hi)
+
+# I added Player class only, still thinking what to do
+class Actor:
+    def __init__(self, name: str):
+        self.name = name
+        self.score = 0
+
+    def add_score(self, pt: int):
+        self.score += pt
+
+    def minus_score(self, pt: int):
+        self.score -= pt
+
+class Player(Actor):
+    def __init__(self, name: str, score: int = 0):
+        super().__init__(name)
+        self.score = score
